e23d79ab08ee3a42d56455f5f847b9291328cb15
ahtalbi <obetox1@gmail.com>
2025-09-09 09:33:44 +0100

    filter element from array

---
+++ JavaScript/FilterElementsfromArray.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var filter = function (arr, fn) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+};
+
+console.log(filter([0, 10, 20, 30], (x) => x > 10));
