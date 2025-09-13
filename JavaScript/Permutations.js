03e891a36e0c34318545726b6516642148f0a937
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-09-13 02:03:55 +0100

    Permùutations

--- JavaScript/Permutations.js
+++ JavaScript/Permutations.js
@@ -1,14 +1,21 @@
-
-var permute = function (ar) {
-
-    function backtrack(arr, permu) {
-        if (arr.len === 0) {
-            
+function permute(nums) {
+    let res = [], used = {}
+    function solve(arr) {
+        if (arr.length === nums.length) {
+            res.push([...arr]);
+            return;
+        }
+        for (let i = 0;i < nums.length; i++) {
+            if (used[nums[i]]) continue;
+            arr.push(nums[i]);
+            used[nums[i]] = true
+            solve(arr)
+            used[nums[i]] = false
+            arr.pop(nums[i]);
         }
     }
-    backtrack(ar,[])
+    solve([]);
+    return res;
 };
 
-console.log(permute([1, 2, 3]));
-fff
-// [1,1,2]
+console.log(permute(9 , 24479));
