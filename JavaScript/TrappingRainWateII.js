0939183f10f3fc5d46155fb3e7c4de961336ac0c
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-10-03 23:13:31 +0100

    traping rain water II

---
+++ JavaScript/TrappingRainWateII.js
@@ -0,0 +1,11 @@
+var trapRainWater = function(hM) {
+    let q = []
+    for (let i = 0; i < hM.length; i++) {
+        for (let j = 0; j < hM[i].length; j++) {
+            
+        }
+        console.log(hM[i]);
+    }
+};
+
+console.log(trapRainWater([[3,3,3,3,3],[3,2,2,2,3],[3,2,1,2,3],[3,2,2,2,3],[3,3,3,3,3]]));
